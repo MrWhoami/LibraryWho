7911b272bde23895f79335b1a1da73cae4d30686
Whoami0117 <liujiyuan126@yeah.net>
2014-12-15 18:19:52 +0800

    Add reader.cpp

---
+++ LibraryWho/reader.cpp
@@ -0,0 +1,9 @@
+//
+//  reader.cpp
+//  LibraryWho
+//
+//  Created by Whoami on 14/12/15.
+//  Copyright (c) 2014年 Whoami.
+//
+
+#include "reader.h"
---
+++ LibraryWho/reader.h
@@ -0,0 +1,14 @@
+//
+//  reader.h
+//  LibraryWho
+//
+//  Created by Whoami on 14/12/15.
+//  Copyright (c) 2014年 Whoami.
+//
+
+#ifndef __LibraryWho__reader__
+#define __LibraryWho__reader__
+
+#include <stdio.h>
+
+#endif /* defined(__LibraryWho__reader__) */
